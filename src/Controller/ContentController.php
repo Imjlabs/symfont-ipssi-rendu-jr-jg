25988bd929674c9f5dd77749390697df8e851897
jonathan-gu <jonathanguerpillon@gmail.com>
2022-12-01 17:44:59 +0100

    adding date homepage and adding function getProducts

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/')]
 class ContentController extends AbstractController
 {
-    #[Route('', name: 'app_content')]
+    #[Route('', name: 'app_home')]
     public function index(ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findByCreatedDate(3);
 
         return $this->render('content/index.html.twig', [
             'articles' => $articles
+        ]);
+    }
+
+    #[Route('/products', name: 'app_products')]
+    public function getProducts(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('content/products.html.twig', [
+            'products' => $products
         ]);
     }
 }
